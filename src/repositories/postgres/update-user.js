8530d568eebfcd6921a2ebda58c7c8d4bee98aa4
Jose Lima <jalimag0206@gmail.com>
2023-11-14 03:01:44 -0300

    fix: update import helper.js and correcting class name in PostgresUpdateUserRepository

--- src/repositories/postgres/update-user.js
+++ src/repositories/postgres/update-user.js
@@ -1,6 +1,6 @@
-import { PostgresHelper } from '../../db/postgres/helper'
+import { PostgresHelper } from '../../db/postgres/helper.js'
 
-export class PostgresUpdateRepository {
+export class PostgresUpdateUserRepository {
   async execute(userId, updateUserParams) {
     const updateFields = []
     const updateValues = []
