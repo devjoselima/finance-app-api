b7aed960af79105336392c5732e469c614fdd589
Jose Lima <jalimag0206@gmail.com>
2023-11-14 03:13:29 -0300

    feat: create patch route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config.js'
 import express from 'express'
 import { CreateUserController } from './src/controllers/create-user.js'
 import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
+import { UpdateUserController } from './src/controllers/update-user.js'
 
 const app = express()
 
@@ -13,6 +14,14 @@ app.post('/api/users', async (request, response) => {
   const { statusCode, body } = await createUserController.execute(request)
 
   response.status(statusCode).send(body)
+})
+
+app.patch('/api/users/:userId', async (request, reponse) => {
+  const updateUserController = new UpdateUserController()
+
+  const { statusCode, body } = await updateUserController.execute(request)
+
+  reponse.status(statusCode).send(body)
 })
 
 app.get('/api/users/:userId', async (request, response) => {
