49a413e5d2b3f077c80e9b5a90b184fb8f055320
Jose Lima <jalimag0206@gmail.com>
2023-11-17 03:12:51 -0300

    fix: fix SQL syntax change "RETURN" to "RETURNING"

--- src/repositories/postgres/delete-user.js
+++ src/repositories/postgres/delete-user.js
@@ -3,7 +3,7 @@ import { PostgresHelper } from '../../db/postgres/helper.js'
 export class PostgresDeleteUserRepository {
   async execute(userId) {
     const deletedUser = await PostgresHelper.query(
-      'DELETE FROM users WHERE id = $1 RETURN * ',
+      'DELETE FROM users WHERE id = $1 RETURNING * ',
       [userId],
     )
     return deletedUser[0]
