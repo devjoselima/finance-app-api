ee6bd9932b2df6544194d293f517b5514cb0f015
Jose Lima <jalimag0206@gmail.com>
2023-11-20 03:25:51 -0300

    feat: create CreateTransactionController

---
+++ src/controllers/transaction/create-transaction.js
@@ -0,0 +1,68 @@
+import validator from 'validator'
+
+import {
+  serverError,
+  badRequest,
+  checkIfIdIsValid,
+  invalidIdResponse,
+  created,
+} from '../helpers/index.js'
+
+export class CreateTransactionController {
+  constructor(createTransactionUseCase) {
+    this.createTransactionUseCase = createTransactionUseCase
+  }
+  async execute(httpRequest) {
+    try {
+      const params = httpRequest.body
+
+      const requiredFields = ['id', 'user_id', 'name', 'date', 'amount', 'type']
+
+      for (const field of requiredFields) {
+        if (!params[field] || params[field].trim().length === 0) {
+          return badRequest({ message: `Missing param: ${field}` })
+        }
+      }
+
+      const userIdIsValid = checkIfIdIsValid(params.user_id)
+
+      if (!userIdIsValid) {
+        return invalidIdResponse()
+      }
+
+      if (params.amount <= 0) {
+        return badRequest({ message: 'The amount must be greater than 0.' })
+      }
+
+      const amountIsValid = validator.isCurrency(params.amount.toString(), {
+        digits_after_decimal: [2],
+        allow_decimal: false,
+        decimal_separator: '.',
+      })
+
+      if (!amountIsValid) {
+        return badRequest({ message: 'The amount must be a valid currency.' })
+      }
+
+      const type = params.type.trim().toUpperCase()
+
+      const typeIsValid = ['EARNING', 'EXPENSIVE', 'INVESTMENT'].includes(type)
+
+      if (!typeIsValid) {
+        return badRequest({
+          message: 'The type must be EARNING, EXPENSIVE or INVESTMENT',
+        })
+      }
+
+      const transaction = await this.createTransactionUseCase.execute({
+        ...params,
+        type,
+      })
+
+      return created(transaction)
+    } catch (error) {
+      console.error(error)
+      return serverError()
+    }
+  }
+}
