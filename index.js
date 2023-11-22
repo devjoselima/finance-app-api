7622f8c0c013a2ac9203b699f15a8e49ed6f94bf
Jose Lima <jalimag0206@gmail.com>
2023-11-22 20:29:49 -0300

    feat: add route to update a transaction

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import {
 import {
     makeCreateTransactionController,
     makeGetTransactionByUserIdController,
+    makeUpdateTransactionController,
 } from './src/factories/controllers/transaction.js'
 
 const app = express()
@@ -64,6 +65,15 @@ app.post('/api/transactions', async (request, response) => {
         await createTransactionController.execute(request)
 
     console.log(statusCode)
+
+    response.status(statusCode).send(body)
+})
+
+app.patch('/api/transactions/:transactionId', async (request, response) => {
+    const updateTransactionController = makeUpdateTransactionController()
+
+    const { statusCode, body } =
+        await updateTransactionController.execute(request)
 
     response.status(statusCode).send(body)
 })
