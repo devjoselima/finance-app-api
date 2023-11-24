f821666452b16e2c2c98f003695e5a7301efd8fe
Jose Lima <jalimag0206@gmail.com>
2023-11-24 01:19:29 -0300

    feat: add DeleteTransactionUseCase

---
+++ src/use-cases/transaction/delete-transaction.js
@@ -0,0 +1,11 @@
+export class DeleteTransactionUseCase {
+    constructor(deleteTransactionRepository) {
+        this.deleteTransactionRepository = deleteTransactionRepository
+    }
+    async execute(transactionId) {
+        const transaction =
+            await this.deleteTransactionRepository.execute(transactionId)
+
+        return transaction
+    }
+}
