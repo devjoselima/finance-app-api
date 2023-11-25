cbdeb7a2223fd1250bcd7128496c7360286f40d5
Jose Lima <jalimag0206@gmail.com>
2023-11-24 22:49:37 -0300

    feat: add GetUserBalanceController

---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,36 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import {
+    userNotFoundResponse,
+    serverError,
+    checkIfIdIsValid,
+    invalidIdResponse,
+    success,
+} from '../helpers/index.js'
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId
+
+            const idIsValid = checkIfIdIsValid(userId)
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const balance = await this.getUserBalanceUseCase.execute({ userId })
+
+            return success(balance)
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse
+            }
+            console.error(error)
+            return serverError
+        }
+    }
+}
