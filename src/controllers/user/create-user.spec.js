115130966ebcf46c186f4f7253e222dff0454c43
Jose Lima <jalimag0206@gmail.com>
2024-02-17 13:30:31 -0300

    test: ensure 201 is return when user is created

---
+++ src/controllers/user/create-user.spec.js
@@ -0,0 +1,30 @@
+import { CreateUserController } from './create-user.js'
+
+import { describe, it, expect } from '@jest/globals'
+
+describe('Create User Controller', () => {
+    class CreateUserUseCaseStub {
+        execute(user) {
+            return user
+        }
+    }
+
+    it('should return 201 when creating a user successfully', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
+                last_name: 'Doe',
+                email: 'johndoe@mail.com',
+                password: '1234567',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(201)
+        expect(result.body).toBe(httpRequest.body)
+    })
+})
