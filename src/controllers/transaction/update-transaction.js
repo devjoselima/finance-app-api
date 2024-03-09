b04dc5515316215bb0247bab4c86a44e5922f83b
Jose Lima <jalimag0206@gmail.com>
2024-03-09 13:45:14 -0300

    feat: add zod to UpdateTransactionController

--- src/controllers/transaction/update-transaction.js
+++ src/controllers/transaction/update-transaction.js
@@ -1,12 +1,10 @@
+import { ZodError } from 'zod'
+import { updateTransactionSchema } from '../../schemas/transaction.js'
 import {
+    badRequest,
     checkIfIdIsValid,
     invalidIdResponse,
     serverError,
-    badRequest,
-    checkIfAmountIsValid,
-    invalidAmountResponse,
-    checkIfTypeIsValid,
-    invalidTypeResponse,
     success,
 } from '../helpers/index.js'
 
@@ -25,33 +23,7 @@ export class UpdateTransactionController {
 
             const params = httpRequest.body
 
-            const allowedFields = ['name', 'date', 'amount', 'type']
-
-            const someFieldIsNotAllowed = Object.keys(params).some(
-                (field) => !allowedFields.includes(field),
-            )
-
-            if (someFieldIsNotAllowed) {
-                return badRequest({
-                    message: 'Some provided field is not allowed',
-                })
-            }
-
-            if (params.amount) {
-                const amountIsValid = checkIfAmountIsValid(params.amount)
-
-                if (!amountIsValid) {
-                    return invalidAmountResponse()
-                }
-            }
-
-            if (params.type) {
-                const typeIsValid = checkIfTypeIsValid(params.type)
-
-                if (!typeIsValid) {
-                    return invalidTypeResponse()
-                }
-            }
+            await updateTransactionSchema.parseAsync(params)
 
             const transaction = await this.updateTransactionUseCase.execute(
                 httpRequest.params.transactionId,
@@ -60,6 +32,12 @@ export class UpdateTransactionController {
 
             return success(transaction)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
+
             console.error(error)
 
             return serverError()
--- src/schemas/transaction.js
+++ src/schemas/transaction.js
@@ -49,3 +49,10 @@ export const createTransactionSchema = z.object({
             }),
         ),
 })
+
+export const updateTransactionSchema = createTransactionSchema
+    .omit({ user_id: true })
+    .partial()
+    .strict({
+        message: 'Some provided field is not allowed',
+    })
